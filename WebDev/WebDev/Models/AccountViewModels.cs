62552e8b83c373b9ad92a43613b349aefc6af61f
Nuno Cunha (1110621) <1110621@isep.ipp.pt>
2014-12-14 12:20:16 +0000

    Added field to the user registration model, so a user can pick his/her own username (ref #16)

--- WebDev/WebDev/Models/AccountViewModels.cs
+++ WebDev/WebDev/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace WebDev.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(32, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
