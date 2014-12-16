e2a47c835d69122b601c1d882b2fc72a1ce2fe04
Nuno Cunha (1110621) <1110621@isep.ipp.pt>
2014-12-16 13:00:13 +0000

    Created database to validate users on the service (ref #16, ref #23)

---
+++ WebDev/WebDev/DAL/WebDevInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebDev.Models;
+
+namespace WebDev.DAL
+{
+    public class WebDevInitializer : DropCreateDatabaseIfModelChanges<WebDevContext>
+    {
+        protected override void Seed(WebDevContext context)
+        {
+            var users = new List<User> {
+                new User {
+                    Username = "1110621",
+                    PasswordHash = "B50D9DA273363ECCEF03008B2099D3EC"
+                }
+            };
+
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
+        }
+    }
+}
