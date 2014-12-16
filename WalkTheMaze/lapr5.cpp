21dc7cb4fe97eeaa3080f29b27ebe639e8bca6ef
Paulo Oliveira <pajolool1471@gmail.com>
2014-12-16 21:47:22 +0000

    Login method created ref #11

--- WalkTheMaze/lapr5.cpp
+++ WalkTheMaze/lapr5.cpp
@@ -1026,15 +1026,7 @@ void mouse(int btn, int state, int x, int y){
 //	glutPostRedisplay();
 //}
 
-bool login() {
-	/*string username = "", password = "";
-	cout << "******* SIGN IN *******\n";
-	cout << "Username: ";
-	getline(cin, username);
-	cout << "Password: ";
-	getline(cin, password);
-	cout << "Username: " << username << " Password: " << password;*/
-
+int callLoginService(string user, string pass) {
 	HRESULT hr = ERROR_SUCCESS;
 	WS_ERROR* error = NULL;
 	WS_HEAP* heap = NULL;
@@ -1046,21 +1038,48 @@ bool login() {
 	address.url = url;
 	hr = WsCreateHeap(2048, 512, NULL, 0, &heap, error);
 	WS_HTTP_BINDING_TEMPLATE templ = {};
-
 	// criação do proxy para o serviço
 	hr = BasicHttpBinding_IService_CreateServiceProxy(
 		&templ, NULL, 0, &proxy, error);
 	hr = WsOpenServiceProxy(proxy, &address, NULL, error);
 
-	//	_Login login;
-	WCHAR* var;
+	WCHAR* idResult;
 	WCHAR username = L'username';
 	WCHAR password = L'password';
 	hr = BasicHttpBinding_IService_Login(
-		proxy, &username, &password, &var, heap, NULL, 0, NULL, error);
-	wprintf(L"%s\n", var);
+		proxy, &username, &password, &idResult, heap, NULL, 0, NULL, error);
+	//wprintf(L"%s\n", idResult);
 
+	int id = _wtoi(idResult);
+
+	return id;
+}
+
+bool login() {
+	string username = "", password = "";
+	bool valid = false;
+	cout << "******* SIGN IN *******\n";
+	while (valid == false) {
+		cout << "Username: ";
+		getline(cin, username);
+		cout << "Password: ";
+		getline(cin, password);
+		cout << "Username: " << username << " Password: " << password;
+		cout << "\n" << endl;
+
+		int id = callLoginService(username, password);
+
+		if (id > 0) { // valid user
+			valid = true;
+			cout << "\n ** You're now logged in !! **\n" << endl;
+			valid = true;
+		}
+		else {
+			cout << "Invalid user, please try again !! \n" << endl;
+		}
+	}
 	return true;
+
 }
 
 void main(int argc, char **argv)
