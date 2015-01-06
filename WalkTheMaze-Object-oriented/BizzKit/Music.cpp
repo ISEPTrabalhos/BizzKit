fc01d7cb043446e44f865d30194f1cf14183c0e3
Mario Garrido <mrfhitz@gmail.com>
2015-01-06 23:55:11 +0000

    Add music class finding the way to fix the sound problem

---
+++ WalkTheMaze-Object-oriented/BizzKit/Music.cpp
@@ -0,0 +1,34 @@
+#include "Music.h"
+
+Music::Music(string file){
+	alGetError();
+	choseMusic(file);
+}
+
+ALboolean Music::choseMusic(string file){
+	// Load pcm data into buffer
+	buffer = alutCreateBufferFromFile(file.c_str());
+
+	// Create sound source (use buffer to fill source)
+	alGenSources(1, &source);
+	alSourcei(source, AL_BUFFER, buffer);
+
+	// Play
+	alSourcePlay(source);
+
+	return (alGetError() != AL_NO_ERROR) ? AL_FALSE : AL_TRUE;
+}
+
+void Music::play(){
+	alSourcePlay(source);
+	status = AL_TRUE;
+}
+
+void Music::stop(){
+	alSourceStop(source);
+	status = AL_FALSE;
+}
+
+void Music::toggle() {
+	(status) ? stop() : play();
+}
---
+++ WalkTheMaze-Object-oriented/BizzKit/Music.h
@@ -0,0 +1,30 @@
+#ifndef _MUSIC
+#define _MUSIC
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <conio.h>
+#include <stdlib.h>
+#include <AL/alut.h>
+#include <stdio.h>
+
+using namespace std;
+
+class Music {
+public:
+	ALuint buffer, source;
+	ALboolean status;
+
+	Music(string file);
+	~Music();
+
+	//methods
+	ALboolean choseMusic(string file);
+	void play();
+	void stop();
+	void toggle();
+};
+
+#endif
