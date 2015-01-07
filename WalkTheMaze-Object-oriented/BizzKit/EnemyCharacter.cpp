f5544ab14461e80a4519d1817791871274f1d563
Nuno Cunha (1110621) <1110621@isep.ipp.pt>
2015-01-07 20:42:29 +0000

    Added animation to enemy (ref #31)

--- WalkTheMaze-Object-oriented/BizzKit/EnemyCharacter.cpp
+++ WalkTheMaze-Object-oriented/BizzKit/EnemyCharacter.cpp
@@ -4,9 +4,10 @@
 EnemyCharacter::EnemyCharacter(){
     position = new Point();
 	mdlviewer_init("mummy.mdl", model);
-    position->x = -250;
-    position->y = -250;
-    position->z = 0;
+	model.SetSequence(1);
+    position->x = -240;
+    position->y = -240;
+	position->z = CHARACTER_HEIGHT / 2;
     dir = 0;
     vel = 0.5;
 	health = INT_MAX;
