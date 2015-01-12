8d0f5545e92c693ab567f7687aa4bc2f3085e1cc
António Silva <antoniosilva1017@gmail.com>
2015-01-12 09:55:49 +0000

    Added the download page and the download method @mrfhitz #51

---
+++ WebDev/WebDev/Controllers/DownloadsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDev.DAL;
+
+namespace WebDev.Controllers
+{
+    public class DownloadsController : Controller
+    {
+        // GET: Downloads
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        // GET: Downloads/Download
+        public ActionResult Download()
+        {
+            /*WebDevContext db = new WebDevContext();
+            var download = db.Downloads.Find(1);
+            download.quantity++;
+            db.SaveChanges();*/
+            Response.Redirect("#");
+            return View();
+        }
+    }
+}
