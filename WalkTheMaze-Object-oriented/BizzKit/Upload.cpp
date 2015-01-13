f939bc1afb9edca519418c3c6580bff481745d05
lapr4_1110738 <1110738@isep.ipp.pt>
2015-01-13 10:03:19 +0000

    Changed the parameters of the upload progress methods #40

--- WalkTheMaze-Object-oriented/BizzKit/Upload.cpp
+++ WalkTheMaze-Object-oriented/BizzKit/Upload.cpp
@@ -1,10 +1,9 @@
 #include "Upload.h"
 #include "ServicesHandler.h"
 
-int Upload::uploadProgress(string username, int positions [])
+int Upload::uploadProgress(string username, int score, GLfloat pos_x, GLfloat pos_y)
 {
 	ServicesHandler *service = new ServicesHandler;
-	int res = service->uploadRoute(username, positions);
+	int res = service->uploadRoute(username, score, pos_x, pos_y);
 	return res;
 }
-
