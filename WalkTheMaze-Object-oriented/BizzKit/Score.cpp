36eff1bb143b0855d7a6e8dd4e8fd5b08e81e47e
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-17 17:56:40 +0000

    Remove unused classes

--- WalkTheMaze-Object-oriented/BizzKit/Score.cpp
+++
@@ -1,9 +0,0 @@
-#include "Score.h"
-#include "ServicesHandler.h"
-
-int Score::uploadScore(string username,string levelName, int score)
-{
-	ServicesHandler *service = new ServicesHandler();
-	int points = service->score(username, levelName, score);
-	return points;
-}
