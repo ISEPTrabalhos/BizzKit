b8013836ac404ac4b0b5792d02ba87268cdeb8cf
António Silva <antoniosilva1017@gmail.com>
2015-01-18 20:54:18 +0000

    Rework on the page design and added some more translations #63

--- WebDev/WebDev/Controllers/HomeController.cs
+++ WebDev/WebDev/Controllers/HomeController.cs
@@ -15,19 +15,5 @@ namespace WebDev.Controllers
             //var t = from u in db.Textures select u;
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
--- WebDev/WebDev/Resources/Resource.Designer.cs
+++ WebDev/WebDev/Resources/Resource.Designer.cs
@@ -70,11 +70,47 @@ namespace WebDev.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Download.
+        /// </summary>
+        public static string Download {
+            get {
+                return ResourceManager.GetString("Download", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Download the game.
+        /// </summary>
+        public static string DownloadHere {
+            get {
+                return ResourceManager.GetString("DownloadHere", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Downloads.
         /// </summary>
         public static string Downloads {
             get {
                 return ResourceManager.GetString("Downloads", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to File name.
+        /// </summary>
+        public static string FileName {
+            get {
+                return ResourceManager.GetString("FileName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Game.
+        /// </summary>
+        public static string Game {
+            get {
+                return ResourceManager.GetString("Game", resourceCulture);
             }
         }
         
@@ -93,6 +129,33 @@ namespace WebDev.Resources {
         public static string Home {
             get {
                 return ResourceManager.GetString("Home", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Walk the Maze is a free and new Game, it displays a large maze that you need to get out, but be carefull or the monsters will kill you.
+        /// </summary>
+        public static string HomeSubTitle {
+            get {
+                return ResourceManager.GetString("HomeSubTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to How to play.
+        /// </summary>
+        public static string HowToPlay {
+            get {
+                return ResourceManager.GetString("HowToPlay", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to First you register for a new account, then you download the game, unzip it and you are good to go.
+        /// </summary>
+        public static string HowToPlayGuide {
+            get {
+                return ResourceManager.GetString("HowToPlayGuide", resourceCulture);
             }
         }
         
@@ -165,6 +228,24 @@ namespace WebDev.Resources {
         public static string Score {
             get {
                 return ResourceManager.GetString("Score", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Start you&apos;re download by pressing here.
+        /// </summary>
+        public static string StartDownload {
+            get {
+                return ResourceManager.GetString("StartDownload", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Username.
+        /// </summary>
+        public static string Username {
+            get {
+                return ResourceManager.GetString("Username", resourceCulture);
             }
         }
     }
